54740c7c8b55955d7c85c9c22ff021b112f919f6
Mohit Ashliya <mohit.ashliya@gmail.com>
2023-11-07 10:31:43 +0530

    added the Income type in adding income to database form

--- src/pages/ExpenseTracker.jsx
+++ src/pages/ExpenseTracker.jsx
@@ -1,7 +1,9 @@
 import React, { useState, Fragment, useEffect } from 'react'
 import { motion } from 'framer-motion'
 import BasicTabs from '../components/ExpenseTabs'
-import { BsPlusCircle } from 'react-icons/bs'
+import { BsPlusCircle, BsCash, BsBank2 } from 'react-icons/bs'
+import { RxCrossCircled } from 'react-icons/rx'
+import { PiCurrencyBtc } from 'react-icons/pi'
 // Rerenders onload  Not a child of App.jsx
 // used material ui here 
 import * as Yup from 'yup'
@@ -38,9 +40,16 @@ const ExpenseTracker = () => {
         { name: "Other" }
     ];
 
+    const incomeTypes = {
+        cash: 'cash',
+        bank: 'bank',
+        bitcoin: 'bitcoin'
+    }
+
     const [addIncomeForm, setAddIncomeForm] = useState(true)
     const [enabled, setEnabled] = useState(false)
     const [selected, setSelected] = useState(expenseCategories[0])
+    const [incomeType, setIncomeType] = useState(incomeTypes.cash)
     const formik = useFormik({
         validationSchema: inputSchema,
         initialValues: {
@@ -85,7 +94,14 @@ const ExpenseTracker = () => {
                     exit={{ x: "100%", opacity: 0 }}
                     transition={{ duration: 0.4 }}
                 >
-                    <div className='bg-secondary w-[25rem] p-4 h-auto rounded-lg flex justify-center items-center'>
+                    <div className='bg-secondary w-[25rem] p-4 h-auto rounded-lg flex justify-center items-center relative'>
+                        <button onClick={() => {
+                            setAddIncomeForm(false)
+                            setEnabled(false)
+                            setIncomeType(incomeTypes.cash)
+                        }} className='absolute -top-8 right-0'>
+                            <RxCrossCircled size={30} />
+                        </button>
                         <form className='p-4' onSubmit={formik.handleSubmit}>
                             <label htmlFor="amount">Amount ₹</label>
                             <input
@@ -97,57 +113,62 @@ const ExpenseTracker = () => {
                                 className='w-full p-4 rounded-sm shadow-lg mb-4 focus:outline-none'
                                 type="text" />
                             <br />
-                            <label htmlFor="category">Add Category</label>
-                            <Listbox value={selected} onChange={setSelected}>
-                                <div className="relative mt-1 bg-background w-full p-3 shadow-md">
-                                    <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-                                        <span className="block truncate">{selected.name}</span>
-                                        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                                            <ChevronUpDownIcon
-                                                className="h-5 w-5 text-dark"
-                                                aria-hidden="true"
-                                            />
-                                        </span>
-                                    </Listbox.Button>
-                                    <Transition
-                                        as={Fragment}
-                                        leave="transition ease-in duration-100"
-                                        leaveFrom="opacity-100"
-                                        leaveTo="opacity-0"
-                                    >
-                                        <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-lg font-semibold shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm bg-background z-[13]">
-                                            {expenseCategories.map((person, personIdx) => (
-                                                <Listbox.Option
-                                                    key={personIdx}
-                                                    className={({ active }) =>
-                                                        `relative cursor-default select-none py-2 px-2 bg-background hover:bg-accent hover:text-background transition-all ease-in-out saturate-150`
-                                                    }
-                                                    value={person}
-                                                >
-                                                    {({ selected }) => (
-                                                        <>
-                                                            <span
-                                                                className={`block truncate ${selected ? 'font-medium' : 'font-normal'
-                                                                    }`}
-                                                            >
-                                                                {person.name}
-                                                            </span>
-                                                            {selected ? (
-                                                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                                                                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                                                </span>
-                                                            ) : null}
-                                                        </>
-                                                    )}
-                                                </Listbox.Option>
-                                            ))}
-                                        </Listbox.Options>
-                                    </Transition>
-                                </div>
-                            </Listbox>
+                            {
+                                !enabled &&
+                                <>
+                                    <label htmlFor="category">Add Category</label>
+                                    <Listbox value={selected} onChange={setSelected}>
+                                        <div className="relative mt-1 bg-background w-full p-3 shadow-md">
+                                            <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                                                <span className="block truncate">{selected.name}</span>
+                                                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                                                    <ChevronUpDownIcon
+                                                        className="h-5 w-5 text-dark"
+                                                        aria-hidden="true"
+                                                    />
+                                                </span>
+                                            </Listbox.Button>
+                                            <Transition
+                                                as={Fragment}
+                                                leave="transition ease-in duration-100"
+                                                leaveFrom="opacity-100"
+                                                leaveTo="opacity-0"
+                                            >
+                                                <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-lg font-semibold shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm bg-background z-[13]">
+                                                    {expenseCategories.map((person, personIdx) => (
+                                                        <Listbox.Option
+                                                            key={personIdx}
+                                                            className={({ active }) =>
+                                                                `relative cursor-default select-none py-2 px-2 bg-background hover:bg-accent hover:text-background transition-all ease-in-out saturate-150`
+                                                            }
+                                                            value={person}
+                                                        >
+                                                            {({ selected }) => (
+                                                                <>
+                                                                    <span
+                                                                        className={`block truncate ${selected ? 'font-medium' : 'font-normal'
+                                                                            }`}
+                                                                    >
+                                                                        {person.name}
+                                                                    </span>
+                                                                    {selected ? (
+                                                                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                                                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                                        </span>
+                                                                    ) : null}
+                                                                </>
+                                                            )}
+                                                        </Listbox.Option>
+                                                    ))}
+                                                </Listbox.Options>
+                                            </Transition>
+                                        </div>
+                                    </Listbox>
+                                </>
+                            }
                             <br />
                             {
-                                selected.name === 'Other' &&
+                                (selected.name === 'Other' && !enabled) &&
                                 (<>
                                     <label htmlFor="other"></label>
                                     <input
@@ -160,6 +181,14 @@ const ExpenseTracker = () => {
                                         type="text" />
                                     <br />
                                 </>)
+                            }
+                            {
+                                enabled &&
+                                <div className='w-full mb-3 flex justify-between'>
+                                    <button onClick={() => setIncomeType(incomeTypes.cash)} style={{ background: `${incomeType === incomeTypes.cash ? '#77FF94' : '#FFF'}` }} className='p-4 bg-background rounded-md hover:-translate-y-2 transition-all ease-in-out duration-150 hover:shadow-lg'><BsCash size={30} /></button>
+                                    <button onClick={() => setIncomeType(incomeTypes.bank)} style={{ background: `${incomeType === incomeTypes.bank ? '#77FF94' : '#FFF'}` }} className='p-4 bg-background rounded-md hover:-translate-y-2 transition-all ease-in-out duration-150 hover:shadow-lg'><BsBank2 size={30} /></button>
+                                    <button onClick={() => setIncomeType(incomeTypes.bitcoin)} style={{ background: `${incomeType === incomeTypes.bitcoin ? '#77FF94' : '#FFF'}` }} className='p-4 bg-background rounded-md hover:-translate-y-2 transition-all ease-in-out duration-150 hover:shadow-lg'><PiCurrencyBtc size={30} /></button>
+                                </div>
                             }
                             <div className='w-full flex justify-between items-center mb-4'>
                                 <Switch
